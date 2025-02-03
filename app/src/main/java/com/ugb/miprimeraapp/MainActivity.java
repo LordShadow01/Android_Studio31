3fa337b032c6302877d35ec1da07ae8f1a642e19
LordShadow01 <rosalesjonathan2024@gmail.com>
2025-02-02 18:33:16 -0600

    calculadora de sumar,restar,multiplicacion,division

--- app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.ugb.miprimeraapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +16,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView temVal;
+    RadioGroup rgb;
 
+    RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +34,24 @@ public class MainActivity extends AppCompatActivity {
                 temVal = findViewById(R.id.txtNum2);
                 double num2 = Double.parseDouble(temVal.getText().toString());
 
-                double respuesta = num1 + num2;
+                double respuesta = 0.0;
+
+                opt = findViewById(R.id.optSuma);
+                if (opt.isChecked()) {
+                    respuesta = num1 + num2;
+                }
+                opt = findViewById(R.id.optResta);
+                if (opt.isChecked()) {
+                    respuesta = num1 - num2;
+                }
+                opt = findViewById(R.id.optMultiplicacion);
+                if (opt.isChecked()) {
+                    respuesta = num1 * num2;
+                }
+                opt = findViewById(R.id.optDivision);
+                if (opt.isChecked()) {
+                    respuesta = num1 / num2;
+                }
 
                 temVal= findViewById(R.id.lblRespuesta);
                 temVal.setText("Respuesta: "+ respuesta);
