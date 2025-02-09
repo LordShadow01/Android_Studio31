1ed977f1e09f540199c804c2d5097e52f2e7e50e
LordShadow01 <rosalesjonathan2024@gmail.com>
2025-02-08 18:52:06 -0600

    Uso del Spinner,Msg y array

--- app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -3,10 +3,11 @@ package com.ugb.miprimeraapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,86 +16,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     Button btn;
-    TextView tempVal; // like flames
-    RadioGroup rgb; // oh no,oh si xd
-    RadioButton opt; // storyteller
-    EditText num1, num2;
-
+    TextView tempVal;
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnCalcular);
-        num1 = findViewById(R.id.txtNum1);
-        num2 = findViewById(R.id.txtNum2);
-        rgb = findViewById(R.id.rgoOpciones);
-
-        rgb.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.optRaiz || checkedId == R.id.optFactorial || checkedId == R.id.optCubica) {
-                    num2.setText("");
-                    num2.setEnabled(false);
-                } else {
-                    num2.setEnabled(true);
-                }
-            }
-        });
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tempVal = findViewById(R.id.txtNum1);
                 double num1 = Double.parseDouble(tempVal.getText().toString());
+
                 tempVal = findViewById(R.id.txtNum2);
-                double num2 = tempVal.isEnabled() ? Double.parseDouble(tempVal.getText().toString()) : 0;
+                double num2 = Double.parseDouble(tempVal.getText().toString());
+
                 double respuesta = 0.0;
-
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    respuesta = num1 + num2;
+                String msg = "";
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
+                        respuesta = num1 + num2;
+                        msg = "La suma es: "+ respuesta;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        msg = "La resta es: "+ respuesta;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        msg = "La multiplicacion es: "+ respuesta;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        msg = "La division: "+ respuesta;
+                        break;
                 }
-                opt = findViewById(R.id.optResta);
-                if (opt.isChecked()) {
-                    respuesta = num1 - num2;
-                }
-                opt = findViewById(R.id.optMultiplicacion);
-                if (opt.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-                opt = findViewById(R.id.optDivision);
-                if (opt.isChecked()) {
-                    respuesta = num1 / num2;
-                }
-                opt = findViewById(R.id.optExponente);
-                if (opt.isChecked()) {
-                    respuesta = Math.pow(num1, num2);
-                }
-                opt = findViewById(R.id.optPorcentaje);
-                if (opt.isChecked()) {
-                    respuesta = (num1 * num2) / 100;
-                }
-                opt = findViewById(R.id.optRaiz);
-                if (opt.isChecked()) {
-                    respuesta = Math.sqrt(num1);
-                }
-                opt= findViewById(R.id.optFactorial);
-                if (opt.isChecked()){
-                    respuesta = 1;
-                    for (int i = 2; i <= num1; i++){
-                        respuesta *= i;
-                    }
-                }
-                opt = findViewById(R.id.optCubica); // quiero un cafe
-                if (opt.isChecked()){
-                    respuesta = Math.cbrt(num1);
-                }
-
                 tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: " + respuesta);
+                tempVal.setText("Respuesta: "+ respuesta);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
             }
         });
     }
